59799ef9e940f162b76f068b1569b5cf65c7a317
gnanasekar-mitrah <gnanasekar.p@mitrahsoft.in>
2026-05-27 18:38:37 +0530

    Testing failed test

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -7,3 +7,12 @@ test('Login to saucedemo', async ({page}) => {
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
+
+test('Add item to cart', async ({page}) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Add to cart' }).first().click();
+  await expect(page.getByRole('button', { name: 'Remove' })).not.toBeVisible();
+});
